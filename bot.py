a65cac0be4576501c72ab83c8ecc7aded1a9d33f
Keyyard <84187238+Keyyard@users.noreply.github.com>
2021-08-17 06:40:59 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import datetime
+import discord
+token = "ODc2OTY1MTc3NjA0NTk1NzYy.YRrvdw.b9kIlwD08BkLPYeRK6wXRFi7aCk"
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+    async def on_message(self, message):
+        
+        if message.author.id == self.user.id:
+            return
+            
+        if message.content.startswith('im sleepy'):
+            await message.reply("Hi there, you should set your alarm at: \n", datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=90) datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=180), "\n",datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=270), "\n",datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=360), "\n",datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=450), "\n",datetime.datetime.now()+ datetime.timedelta(minutes=15)+ datetime.timedelta(minutes=540), "\n", mention_author=True)
+client = MyClient()
+client.run('token')
